c762d38de10641429215465ad170ba5f43c945e0
Olivier Perret <Olivier.Perret@mailbox.org>
2017-12-27 12:56:03 +0100

    cleanup image-viewer code
    Cut down on global variables, use closures where possible
    Handle transitions from one command to another by cleaning up leftover state

--- scripts/image-viewer.lua
+++ scripts/image-viewer.lua
@@ -1,25 +1,35 @@
 local needs_adjusting = false
-local video_dimensions = {
-    top_left = { x = 0, y = 0 },
-    bottom_right = { x = 0, y = 0 },
-    size = { w = 0, h = 0 },
-}
-local video_pan_origin = { x = 0, y = 0 }
-local mouse_pos_origin = { x = 0, y = 0 }
-local zoom_origin = 0
+local current_idle = nil
 local zoom_increment = 0
 
 local opts = {
     margin = 50,
     do_not_move_if_all_visible = true,
 }
-local options = require 'mp.options'
-options.read_options(opts)
+(require 'mp.options').read_options(opts)
+
+function register_idle(func)
+    current_idle = func
+    mp.register_idle(current_idle)
+end
+
+function cleanup()
+    mp.remove_key_binding("image-viewer-impl")
+    if current_idle then mp.unregister_idle(current_idle) end
+    needs_adjusting = false
+    zoom_increment = 0
+end
 
 function compute_video_dimensions()
     -- this function is very much ripped from video/out/aspect.c in mpv's source
-    local keep_aspect = mp.get_property_bool("keepaspect")
     local video_params = mp.get_property_native("video-out-params")
+    if not video_params then return nil end
+    local video_dimensions = {
+        top_left = {x = 0, y = 0},
+        bottom_right = {x = 0, y = 0},
+        size = {h = 0, w = 0},
+    }
+    local keep_aspect = mp.get_property_bool("keepaspect")
     local w = video_params["w"]
     local h = video_params["h"]
     local dw = video_params["dw"]
@@ -94,85 +104,64 @@ function compute_video_dimensions()
     end
     video_dimensions.size.w = video_dimensions.bottom_right.x - video_dimensions.top_left.x
     video_dimensions.size.h = video_dimensions.bottom_right.y - video_dimensions.top_left.y
-end
-
-function drag_to_pan_idle()
-    if needs_adjusting then
-        local mX, mY = mp.get_mouse_pos()
-        local pX = video_pan_origin.x + (mX - mouse_pos_origin.x) / video_dimensions.size.w
-        local pY = video_pan_origin.y + (mY - mouse_pos_origin.y) / video_dimensions.size.h
-        mp.command("no-osd set video-pan-x " .. pX .. "; no-osd set video-pan-y " .. pY)
-        needs_adjusting = false
-    end
+    return video_dimensions
 end
 
 function drag_to_pan_handler(table)
+    cleanup()
     if table["event"] == "down" then
-        if not mp.get_property("video-out-params", nil) then return end
-        compute_video_dimensions()
-        mp.register_idle(drag_to_pan_idle)
+        local video_dimensions = compute_video_dimensions()
+        if not video_dimensions then return end
+        local mouse_pos_origin, video_pan_origin = {}, {}
         mouse_pos_origin.x, mouse_pos_origin.y = mp.get_mouse_pos()
         video_pan_origin.x = mp.get_property("video-pan-x")
         video_pan_origin.y = mp.get_property("video-pan-y")
-        mp.add_forced_key_binding("mouse_move", "drag-to-pan-idle", function() needs_adjusting = true end)
-    elseif table["event"] == "up" then
-        mp.remove_key_binding("drag-to-pan-idle")
-        mp.unregister_idle(drag_to_pan_idle)
-        needs_adjusting = false
-    end
-end
-
-function pan_follows_cursor_idle()
-    if needs_adjusting then
-        local mX, mY = mp.get_mouse_pos()
-        local window_w, window_h = mp.get_osd_size()
-        local x = math.min(1, math.max(- 2 * mX / window_w + 1, -1))
-        local y = math.min(1, math.max(- 2 * mY / window_h + 1, -1))
-        local command = ""
-        if (opts.do_not_move_if_all_visible and window_w < video_dimensions.size.w) then
-            command = command .. "no-osd set video-pan-x " .. x * (video_dimensions.size.w - window_w + 2 * opts.margin) / (2 * video_dimensions.size.w) .. ";"
-        elseif mp.get_property_number("video-pan-x") ~= 0 then
-            command = command .. "no-osd set video-pan-x " .. "0;"
-        end
-        if (opts.do_not_move_if_all_visible and window_h < video_dimensions.size.h) then
-            command = command .. "no-osd set video-pan-y " .. y * (video_dimensions.size.h - window_h + 2 * opts.margin) / (2 * video_dimensions.size.h) .. ";"
-        elseif mp.get_property_number("video-pan-y") ~= 0 then
-            command = command .. "no-osd set video-pan-y " .. "0;"
-        end
-        if command ~= "" then
-            mp.command(command)
-        end
-        needs_adjusting = false
+        register_idle(function()
+            if needs_adjusting then
+                local mX, mY = mp.get_mouse_pos()
+                local pX = video_pan_origin.x + (mX - mouse_pos_origin.x) / video_dimensions.size.w
+                local pY = video_pan_origin.y + (mY - mouse_pos_origin.y) / video_dimensions.size.h
+                mp.command("no-osd set video-pan-x " .. pX .. "; no-osd set video-pan-y " .. pY)
+                needs_adjusting = false
+            end
+        end)
+        mp.add_forced_key_binding("mouse_move", "image-viewer-impl",
+            function() needs_adjusting = true end
+        )
     end
 end
 
 function pan_follows_cursor_handler(table)
+    cleanup()
     if table["event"] == "down" then
-        if not mp.get_property("video-out-params", nil) then return end
-        compute_video_dimensions()
-        mp.register_idle(pan_follows_cursor_idle)
-        mp.add_forced_key_binding("mouse_move", "pan-follows-cursor-idle", function() needs_adjusting = true end)
-    elseif table["event"] == "up" then
-        mp.remove_key_binding("pan-follows-cursor-idle")
-        mp.unregister_idle(pan_follows_cursor_idle)
-        needs_adjusting = false
-    end
-end
-
-function cursor_centric_zoom_idle()
-    if needs_adjusting then
-        -- the size in pixels of the (in|de)crement
-        local diffHeight = (2 ^ zoom_increment - 1) * video_dimensions.size.h
-        local diffWidth  = (2 ^ zoom_increment - 1) * video_dimensions.size.w
-
-        -- how far (in percentage of the video size) from the middle the cursor is
-        local rx = (video_dimensions.top_left.x + video_dimensions.size.w / 2 - mouse_pos_origin.x) / (video_dimensions.size.w / 2)
-        local ry = (video_dimensions.top_left.y + video_dimensions.size.h / 2 - mouse_pos_origin.y) / (video_dimensions.size.h / 2)
-
-        local newPanX = (video_pan_origin.x * video_dimensions.size.w + rx * diffWidth / 2) / (video_dimensions.size.w + diffWidth)
-        local newPanY = (video_pan_origin.y * video_dimensions.size.h + ry * diffHeight / 2) / (video_dimensions.size.h + diffHeight)
-        mp.command("no-osd set video-zoom " .. zoom_origin + zoom_increment .. "; no-osd set video-pan-x " .. newPanX .. "; no-osd set video-pan-y " .. newPanY)
-        needs_adjusting = false
+        local video_dimensions = compute_video_dimensions()
+        if not video_dimensions then return end
+        local window_w, window_h = mp.get_osd_size()
+        register_idle(function()
+            if needs_adjusting then
+                local mX, mY = mp.get_mouse_pos()
+                local x = math.min(1, math.max(- 2 * mX / window_w + 1, -1))
+                local y = math.min(1, math.max(- 2 * mY / window_h + 1, -1))
+                local command = ""
+                if (opts.do_not_move_if_all_visible and window_w < video_dimensions.size.w) then
+                    command = command .. "no-osd set video-pan-x " .. x * (video_dimensions.size.w - window_w + 2 * opts.margin) / (2 * video_dimensions.size.w) .. ";"
+                elseif mp.get_property_number("video-pan-x") ~= 0 then
+                    command = command .. "no-osd set video-pan-x " .. "0;"
+                end
+                if (opts.do_not_move_if_all_visible and window_h < video_dimensions.size.h) then
+                    command = command .. "no-osd set video-pan-y " .. y * (video_dimensions.size.h - window_h + 2 * opts.margin) / (2 * video_dimensions.size.h) .. ";"
+                elseif mp.get_property_number("video-pan-y") ~= 0 then
+                    command = command .. "no-osd set video-pan-y " .. "0;"
+                end
+                if command ~= "" then
+                    mp.command(command)
+                end
+                needs_adjusting = false
+            end
+        end)
+        mp.add_forced_key_binding("mouse_move", "image-viewer-impl", function()
+            needs_adjusting = true
+        end)
     end
 end
 
@@ -180,37 +169,44 @@ function cursor_centric_zoom_handler(amt)
     local arg_num = tonumber(amt)
     if not arg_num or arg_num == 0 then return end
     if zoom_increment == 0 then
-        if not mp.get_property("video-out-params", nil) then return end
-        compute_video_dimensions()
+        cleanup()
+        local video_dimensions = compute_video_dimensions()
+        if not video_dimensions then return end
+        local mouse_pos_origin, video_pan_origin = {}, {}
         mouse_pos_origin.x, mouse_pos_origin.y = mp.get_mouse_pos()
         video_pan_origin.x = mp.get_property("video-pan-x")
         video_pan_origin.y = mp.get_property("video-pan-y")
-        zoom_origin = mp.get_property("video-zoom")
-        mp.register_idle(cursor_centric_zoom_idle)
-        mp.add_forced_key_binding("mouse_move", "cursor-centric-zoom-stop", cursor_centric_zoom_stop)
+        local zoom_origin = mp.get_property("video-zoom")
+        -- how far the cursor is form the middle of the video (in percentage)
+        local rx = (video_dimensions.top_left.x + video_dimensions.size.w / 2 - mouse_pos_origin.x) / (video_dimensions.size.w / 2)
+        local ry = (video_dimensions.top_left.y + video_dimensions.size.h / 2 - mouse_pos_origin.y) / (video_dimensions.size.h / 2)
+        register_idle(function()
+            if needs_adjusting then
+                -- the size in pixels of the (in|de)crement
+                local diffHeight = (2 ^ zoom_increment - 1) * video_dimensions.size.h
+                local diffWidth  = (2 ^ zoom_increment - 1) * video_dimensions.size.w
+                local newPanX = (video_pan_origin.x * video_dimensions.size.w + rx * diffWidth / 2) / (video_dimensions.size.w + diffWidth)
+                local newPanY = (video_pan_origin.y * video_dimensions.size.h + ry * diffHeight / 2) / (video_dimensions.size.h + diffHeight)
+                mp.command("no-osd set video-zoom " .. zoom_origin + zoom_increment .. "; no-osd set video-pan-x " .. newPanX .. "; no-osd set video-pan-y " .. newPanY)
+                needs_adjusting = false
+            end
+        end)
+        mp.add_forced_key_binding("mouse_move", "image-viewer-impl", cleanup)
     end
     zoom_increment = zoom_increment + arg_num
     needs_adjusting = true
 end
 
-function cursor_centric_zoom_stop()
-    mp.unregister_idle(cursor_centric_zoom_idle)
-    mp.remove_key_binding("cursor-centric-zoom-stop")
-    needs_adjusting = false
-    zoom_increment = 0
-end
-
 function align_border(x, y)
-    compute_video_dimensions()
-    local video_h, video_w = video_dimensions.size.h, video_dimensions.size.w
+    local video_dimensions = compute_video_dimensions()
     local window_w, window_h = mp.get_osd_size()
     local x,y = tonumber(x), tonumber(y)
     local command = ""
     if x then
-        command = command .. "no-osd set video-pan-x " .. x * (video_w - window_w) / (2 * video_w) .. ";"
+        command = command .. "no-osd set video-pan-x " .. x * (video_dimensions.size.w - window_w) / (2 * video_dimensions.size.w) .. ";"
     end
     if y then
-        command = command .. "no-osd set video-pan-y " .. y * (video_h - window_h) / (2 * video_h) .. ";"
+        command = command .. "no-osd set video-pan-y " .. y * (video_dimensions.size.h - window_h) / (2 * video_dimensions.size.h) .. ";"
     end
     if command ~= "" then
         mp.command(command)
@@ -229,9 +225,8 @@ function rotate_video(amt)
 end
 
 function reset_pan_if_visible()
-    compute_video_dimensions()
+    local video_dimensions = compute_video_dimensions()
     local window_w, window_h = mp.get_osd_size()
-
     local command = ""
     if (window_w >= video_dimensions.size.w) then
         command = command .. "no-osd set video-pan-x 0"
