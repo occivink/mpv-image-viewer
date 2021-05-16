cb64dc65aef5da581bc2187606b69e075b72b0af
Olivier Perret <Olivier.Perret@mailbox.org>
2021-05-16 15:12:03 +0200

    [detect-image] Make options runtime configurable
    And refactor the script somewhat. Still pretty brittle

--- scripts/detect-image.lua
+++ scripts/detect-image.lua
@@ -3,22 +3,10 @@ local opts = {
     command_on_image_loaded="",
     command_on_non_image_loaded="",
 }
-(require 'mp.options').read_options(opts)
-
-if opts.command_on_first_image_loaded == ""
-    and opts.command_on_image_loaded == ""
-    and opts.command_on_non_image_loaded == ""
-then
-    return
-end
-
+local options = require 'mp.options'
 local msg = require 'mp.msg'
 
-local was_image = false
-local frame_count = nil
-local audio_tracks = nil
-local out_params_ready = nil
-local path = nil
+options.read_options(opts, mp.get_script_name(), function() end)
 
 function run_maybe(str)
     if str ~= "" then
@@ -26,64 +14,60 @@ function run_maybe(str)
     end
 end
 
+local was_image = false
+
 function set_image(is_image)
     if is_image and not was_image then
-        msg.verbose("First image detected")
+        msg.info("First image detected")
         run_maybe(opts.command_on_first_image_loaded)
     end
     if is_image then
-        msg.verbose("Image detected")
+        msg.info("Image detected")
         run_maybe(opts.command_on_image_loaded)
     end
     if not is_image and was_image then
-        msg.verbose("Non-image detected")
+        msg.info("Non-image detected")
         run_maybe(opts.command_on_non_image_loaded)
     end
     was_image = is_image
 end
 
-function state_changed()
-    -- only do things when state is consistent
-    if path ~= nil and audio_tracks ~= nil then
-        if frame_count == nil and audio_tracks > 0 then
-            set_image(false)
-        elseif out_params_ready and frame_count ~= nil then
-            -- png have 0 frames, jpg 1 ¯\_(ツ)_/¯
-            set_image((frame_count == 0 or frame_count == 1) and audio_tracks == 0)
+local properties = {}
+
+function properties_changed()
+    local dwidth = properties["dwidth"]
+    local tracks = properties["track-list"]
+    local path = properties["path"]
+    local framecount = properties["estimated-frame-count"]
+
+    if not path or path == "" then return end
+    if not tracks or #tracks == 0 then return end
+    local audio_tracks = 0
+    for _, track in ipairs(tracks) do
+        if track.type == "audio" then
+            audio_tracks = audio_tracks + 1
         end
+    end
+
+    -- only do things when state is consistent
+    if not framecount and audio_tracks > 0 then
+        set_image(false)
+    elseif framecount and dwidth and dwidth > 0 then
+        -- png have 0 frames, jpg 1 ¯\_(ツ)_/¯
+        set_image((framecount == 0 or framecount == 1) and audio_tracks == 0)
     end
 end
 
-mp.observe_property("dwidth", "number", function(_, val)
-    out_params_ready = (val ~= nil and val > 0)
-    state_changed()
-end)
-
-mp.observe_property("estimated-frame-count", "number", function(_, val)
-    frame_count = val
-    state_changed()
-end)
-
-mp.observe_property("path", "string", function(_, val)
-    if not val or val == "" then
-        path = nil
-    else
-        path = val
-    end
-    state_changed()
-end)
-
-mp.register_event("tracks-changed", function()
-    audio_tracks = 0
-    local tracks = 0
-    for _, track in ipairs(mp.get_property_native("track-list")) do
-        tracks = tracks + 1
-         if track.type == "audio" then
-             audio_tracks = audio_tracks + 1
-         end
-    end
-    if tracks == 0 then
-        audio_tracks = nil
-    end
-    state_changed()
-end)
+function observe(propname)
+    mp.observe_property(propname, "native", function(_, val)
+        if val ~= properties[propname] then
+            properties[propname] = val
+            msg.verbose("Property " .. propname .. " changed")
+            properties_changed()
+        end
+    end)
+end
+observe("estimated-frame-count")
+observe("track-list")
+observe("dwidth")
+observe("path")
