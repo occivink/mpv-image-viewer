328a9862b7168bcf26fc87884c85b59ba7125b22
eugenesvk <eugenesvk@users.noreply.github.com>
2023-01-20 22:07:24 +0700

    minimap: reformat

--- scripts/mpvi/4_minimap.lua
+++ scripts/mpvi/4_minimap.lua
@@ -42,15 +42,12 @@ local function split_comma(input)
   end
   return ret
 end
--- msg.info("center	= " .. tostring(opts.center))
 
-local active = false
-local refresh = true
+local active 	= false
+local refresh	= true
 
 local ov	= mp.create_osd_overlay("ass-events")
 
-local function mark_stale()
-  refresh = true
 local function hide_ov()
   ov.data=""
   ov:remove()
@@ -63,6 +60,8 @@ local function draw_ov(asstxt)
   ov.data   = asstxt
   ov:update()
 end
+
+local function mark_stale() refresh = true end
 
 local function refresh_minimap()
   if not refresh then return end
@@ -86,18 +85,19 @@ local function refresh_minimap()
   local draw = function(x, y, w, h, opacity, color)
     a:new_event()
     a:pos(center[1], center[2])
-    a:append("{\\bord0}")
-    a:append("{\\shad0}")
-    a:append("{\\c&" .. color .. "&}")
-    a:append("{\\2a&HFF}")
-    a:append("{\\3a&HFF}")
-    a:append("{\\4a&HFF}")
-    a:append("{\\1a&H" .. opacity .. "}")
+    a:append("{\\bord0}")            	-- border width
+    a:append("{\\shad0}")            	-- shadow width
+    a:append("{\\c&"..color.."&}")   	--
+    a:append("{\\2a&HFF}")           	-- \2a sets the secondary fill alpha (only used for pre-highlight in standard karaoke)
+    a:append("{\\3a&HFF}")           	-- \3a sets the border         alpha
+    a:append("{\\4a&HFF}")           	-- \4a sets the shadow         alpha
+    a:append("{\\1a&H"..opacity.."}")	-- \1a sets the primary   fill alpha
     w = w * 0.5
     h = h * 0.5
     a:draw_start()
     local rounded = {true,true,true,true} -- tl, tr, br, bl
-    local x0,y0,x1,y1 = x-w, y-h, x+w, y+h
+    local x0,y0	= x-w, y-h
+    local x1,y1	= x+w, y+h
     if x0 < -cutoff[1] then
        x0 = -cutoff[1]
       rounded[4] = false
@@ -121,58 +121,42 @@ local function refresh_minimap()
 
     local r = 3
     local c = 0.551915024494 * r
-    if rounded[0] then
-         a:move_to(x0 + r, y0    )
-    else a:move_to(x0    , y0    ) end
-    if rounded[1] then
-         a:line_to(x1 - r, y0    ); a:bezier_curve(x1 - r + c, y0        , x1        , y0 + r - c, x1    , y0 + r)
-    else a:line_to(x1    , y0    ) end
-    if rounded[2] then
-         a:line_to(x1    , y1 - r); a:bezier_curve(x1        , y1 - r + c, x1 - r + c, y1        , x1 - r, y1    )
-    else a:line_to(x1    , y1    ) end
-    if rounded[3] then
-         a:line_to(x0 + r, y1    ); a:bezier_curve(x0 + r - c, y1        , x0        , y1 - r + c, x0    , y1 - r)
-    else a:line_to(x0    , y1    ) end
-    if rounded[4] then
-         a:line_to(x0    , y0 + r); a:bezier_curve(x0        , y0 + r - c, x0 + r - c, y0        , x0 + r, y0    )
-    else a:line_to(x0    , y0    ) end
+    if rounded[0] then a:move_to(x0+r,y0  )
+    else               a:move_to(x0  ,y0  ) end
+    if rounded[1] then a:line_to(x1-r,y0  ); a:bezier_curve(x1-r+c,y0    ,x1    ,y0+r-c,x1  ,y0+r)
+    else               a:line_to(x1  ,y0  ) end
+    if rounded[2] then a:line_to(x1  ,y1-r); a:bezier_curve(x1    ,y1-r+c,x1-r+c,y1    ,x1-r,y1  )
+    else               a:line_to(x1  ,y1  ) end
+    if rounded[3] then a:line_to(x0+r,y1  ); a:bezier_curve(x0+r-c,y1    ,x0    ,y1-r+c,x0  ,y1-r)
+    else               a:line_to(x0  ,y1  ) end
+    if rounded[4] then a:line_to(x0  ,y0+r); a:bezier_curve(x0    ,y0+r-c,x0+r-c,y0    ,x0+r,y0  )
+    else               a:line_to(x0  ,y0  ) end
     a:draw_stop()
   end
-  local image = function()
-    draw(
-      (     dim.ml/2 - dim.mr/2) / opts.scale,
-      (     dim.mt/2 - dim.mb/2) / opts.scale,
-      (ww - dim.ml   - dim.mr  ) / opts.scale,
-      (wh - dim.mt   - dim.mb  ) / opts.scale,
-       opts.image_opacity,
-       opts.image_color)
-  end
-  local view = function()
-    draw(0,
-       0,
-       ww / opts.scale,
-       wh / opts.scale,
-       opts.view_opacity,
-       opts.view_color)
-  end
-  if opts.view_above_image then image();  view()
-  else                           view(); image() end
+  local draw_image = function() draw(
+    (     dim.ml/2 - dim.mr/2) / opts.scale, (     dim.mt/2 - dim.mb/2) / opts.scale,
+    (ww - dim.ml   - dim.mr  ) / opts.scale, (wh - dim.mt   - dim.mb  ) / opts.scale,
+    opts.image_opacity, opts.image_color) end
+  local draw_view = function() draw(
+    0              , 0,
+    ww / opts.scale, wh / opts.scale,
+    opts.view_opacity , opts.view_color ) end
+  if opts.view_above_image then draw_image(); draw_view ()
+  else                          draw_view (); draw_image() end
   draw_ov(a.text)
 end
 
 local function enable()
-  if active then return end
-  active = true
-  mp.observe_property("osd-dimensions", nil, mark_stale)
-  mp.register_idle(refresh_minimap)
+  if     active then return else active = true  end
+  mp.observe_property  ("osd-dimensions"   , nil, mark_stale); mp.register_idle  (refresh_minimap)
+    -- call this whenever OSD dimensions change    ↑  helps to catch OSD init on launch
+    -- delay refresh until all notifications have been received                          ↑
   mark_stale()
 end
 
 local function disable()
-  if not active then return end
-  active = false
-  mp.unobserve_property(mark_stale)
-  mp.unregister_idle(refresh_minimap)
+  if not active then return else active = false end
+  mp.unobserve_property(                          mark_stale); mp.unregister_idle(refresh_minimap)
   hide_ov()
 end
 
