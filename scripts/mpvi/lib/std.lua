8965f48c45fdde3e5dde149dcc7b3a03f8922442
eugenesvk <eugenesvk@users.noreply.github.com>
2023-01-24 15:33:05 +0700

    std: add Math round function

--- scripts/mpvi/lib/std.lua
+++ scripts/mpvi/lib/std.lua
@@ -59,6 +59,24 @@ function std.tlen(T)
   for _ in pairs(T) do count = count + 1 end
   return count
 end
+
+-- Math
+-- Round @ https://stackoverflow.com/a/58411671
+function std.round(num)
+  -- if Lua uses double precision IEC-559 (aka IEEE-754) floats (most do)
+  -- if -2⁵¹ < num < 2⁵¹
+  -- rounding using your FPU's current rounding mode, which is usually round to nearest, ties to even
+  return num + (2^52 + 2^51) - (2^52 + 2^51)
+end
+function std.roundp(num) -- less efficient, performs the same FPU rounding but works for all numbers
+  -- doesn't seem to differ, still fails at 0.5=0
+  local ofs = 2^52
+  if math.abs(num) > ofs then
+    return num
+  end
+  return num < 0 and num - ofs + ofs or num + ofs - ofs
+end
+
 -- MPV-specific functions
 function std.getDimOSD() -- get OSD dimensions only if they exist and wid/height positive
   local dim = mp.get_property_native("osd-dimensions")
