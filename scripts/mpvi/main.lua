55e3bb24d25a76a126c75722ce74ce1614e0b8f6
eugenesvk <eugenesvk@users.noreply.github.com>
2023-01-24 17:33:05 +0700

    main: remove index from names

--- scripts/mpvi/main.lua
+++ scripts/mpvi/main.lua
@@ -8,9 +8,9 @@ function load(relative_path)
 end
 
 -- configs are @ script-opts/mpvi/<script_name>.conf
-load('2_image_positioning'	) -- add several high-level commands to zoom and pan
-load('3_status_line'      	) -- add a status line that can show different properties in the window corner
-load('4_minimap'          	) -- add a minimap that displays the position of the image relative to the view
-load('5_ruler'            	) -- add a `ruler` command that lets you measure positions, distances and angles in the image
-load('6_freeze_window'    	) -- disabled window auto resizing on file changes to fit its size
-load('9_detect_image'     	) -- detect when 🖼 are loaded, allows running commands from .conf
+load('image_positioning'	) -- add several high-level commands to zoom and pan
+load('status_line'      	) -- add a status line that can show different properties in the window corner
+load('minimap'          	) -- add a minimap that displays the position of the image relative to the view
+load('ruler'            	) -- add a `ruler` command that lets you measure positions, distances and angles in the image
+load('freeze_window'    	) -- disabled window auto resizing on file changes to fit its size
+load('detect_image'     	) -- detect when 🖼 are loaded, allows running commands from .conf
