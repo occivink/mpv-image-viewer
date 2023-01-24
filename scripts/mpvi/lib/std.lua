c8da0770f7cc4d768c7faf83d5846c24533b58bf
eugenesvk <eugenesvk@users.noreply.github.com>
2023-01-24 15:34:55 +0700

    std: add YAML config importer

--- scripts/mpvi/lib/std.lua
+++ scripts/mpvi/lib/std.lua
@@ -98,6 +98,116 @@ function std.observe_print(propname)  -- notify on changes, use example ↓
   end)
 end
 
+local parse_yaml	= require "lib/tinyyaml".parse
+function std.read_options_yaml(opts, identifier, on_update)
+  if identifier == nil then identifier = mp.get_script_name() end
+  msg.debug("reading options for "..identifier)
+
+  -- get, read, and parse YAML config file
+  local function read_yaml_config(identifier) -- get YAML config file contet
+    local yaml_ext           	= {'yml','yaml'}
+    local cfgContent,err,cfgF	= nil,"",nil
+    for k, ext in pairs(yaml_ext) do
+      local cfgFileName	= identifier.."."..ext
+      local cfgFilePath	= "script-opts".."/"..cfgFileName
+            cfgF       	= mp.find_config_file(cfgFilePath)
+      if    cfgF == nil then err = err.."failed to find '"..cfgFilePath.."'"  ..'\n'
+      else
+        local  f, e = io.open(cfgF,"r")
+        if     f == nil then err = err.."failed to open '"..cfgF.."' error: "..e..'\n'
+        else cfgContent = f:read("*all"); io.close(f) end
+      end
+    end
+    if err then msg.debug(err) end
+    return cfgContent, cfgF
+  end
+
+  local k_prev,k_top = "","" -- defined outside of ↓ to store recursive compound strings of previous keys
+  local opt_change = {}
+  local function import_opt(cfg_def, cfg_act, cfg_src, change) -- merge old and new configs
+    if change and type(change) ~= 'table' then change = nil
+      local warn = "[ignore] wrong argument type: expected 'table'"..
+        ", got '"..type(change)..
+        "\n  in 'import_opt(...change)' function argument"
+      msg.warn(warn)
+    end
+    local i            	= 0
+    local k_def,k_first	= nil
+    local def_len      	= std.tlen(cfg_def)
+    local act_len      	= std.tlen(cfg_act)
+    for k, v in pairs(cfg_act) do
+      i = i + 1
+      if i == 1 then k_first = k end -- store 1st key to read types from cfg_def if it only has 1 key
+      if     (def_len >= act_len) then k_def = k
+      elseif (def_len > 1       ) then k_def = k
+      else                             k_def = k_first end
+      if   cfg_def[k_def] == nil       then
+        local warn = "[ignore] unknown key '"..k_prev..k.."' = "..tostring(v)
+        if cfg_src then warn = warn.."\n  in '"..cfg_src.."'" end
+        msg.warn(warn)
+      else
+        local  type_def = type(cfg_def[k_def])
+        local  type_cfg = type(v)
+        if     type_def ~= type_cfg then  -- mismatch
+          local warn = "[ignore] wrong key type: "..
+            "expected '"..type_def..
+            "', got '"  ..type_cfg..
+            "' in key '"..k_prev..k.."' = "..tostring(v)
+          if cfg_src then warn = warn.."\n  in '"  ..cfg_src.."'" end
+          msg.warn(warn)
+        elseif type_def == 'table'  then -- → recursively check tables
+          k_prev = k_prev..k.."'→'"
+          if   change then
+               change[k] = v                      -- allows setting change status to leaf keys ↓
+               import_opt(cfg_def[k], cfg_act[k], cfg_src, change[k])
+          else import_opt(cfg_def[k], cfg_act[k], cfg_src           ) end
+        else                             -- or replace config value with parsed
+          if cfg_def[k] ~= v then -- avoids copying
+             cfg_def[k]  = v
+            if change then change[k]  = true  end -- set change status to leaf keys
+          else
+            if change then change[k]  = false end
+          end
+        end
+      end
+    end
+    k_prev = ""
+  end
+
+  -- 1. Import YAML config
+  local cfg_yaml, cfg_file = read_yaml_config(identifier)  -- 1a. read yaml config
+  local cfg_parsed = nil
+  local function try_parse_yaml() cfg_parsed = parse_yaml(cfg_yaml) end -- pcall accepts fn, not fn(arg)
+  if    cfg_yaml ~= nil and not pcall(try_parse_yaml) then msg.warn("failed to parse YAML config")
+  else import_opt(opts, cfg_parsed, cfg_file) end          -- 1b. merge yaml config
+
+  -- 2. Import CLI config (overrides 1.)
+  local cfg_src      	= 'command line'
+  local cfg_parsed   	= nil
+  local cli_prefix   	= identifier.."-"
+  local cli_prefix_re	= cli_prefix:escmagic()
+  local cfg_cli_raw  	= mp.get_property_native("options/script-opts")              -- 2a. read cli opt
+  local function parse_prefix_cfg(cfg, cfg_src)
+    local cfg_cli_parsed	= {}
+    for k, v in pairs(cfg) do
+      if k:startswith(cli_prefix) then cfg_cli_parsed[k:gsub(cli_prefix_re,'',1)]=v end end
+    return cfg_cli_parsed
+  end
+  if   next(cfg_cli_raw) ~= nil then cfg_parsed = parse_prefix_cfg(cfg_cli_raw, cfg_src)
+    if next(cfg_parsed ) ~= nil then import_opt(opts, cfg_parsed, cfg_src) end end -- 2b. merge cli opt
+
+  -- 3. Register auto-update function
+  if on_update then mp.observe_property("options/script-opts", "native", function(name, opt_new)
+    local cfg_src   	= 'on_update'
+    local cfg_parsed	= nil
+    local opt_change	= {}
+    if   next(opt_new   ) ~= nil then cfg_parsed = parse_prefix_cfg(opt_new,cfg_src) -- 3a. merge new opt
+      if next(cfg_parsed) ~= nil then import_opt(opts, cfg_parsed, cfg_src, opt_change) end end
+    if next(opt_change) ~= nil then on_update(opt_change) end
+    end)
+  end
+end
+
 return {
   std = std,
 }
